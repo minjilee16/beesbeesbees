109ada885268f20dfdd7660f434fda1a4ec1c959
Minji Lee <minjilee8816@gmail.com>
2017-05-22 14:37:36 -0700

    done

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,19 @@
 var ForagerBee = function() {
+  Grub.call(this); 
+  this.age = 10; 
+  this.job = 'find pollen';
+  this.color = 'yellow';
+  this.food = 'jelly';
+  this.canFly = true; 
+  this.treasureChest = [];
 };
 
+
+
+ForagerBee.prototype = Object.create(Grub.prototype); 
+ForagerBee.prototype.constructor = ForagerBee; 
+
+
+ForagerBee.prototype.forage= function ( value) {
+  this.treasureChest.push(value); 
+}
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,22 @@
 var HoneyMakerBee = function() {
+  Grub.call(this); 
+  this.age = 10; 
+  this.job = 'make honey';
+  this.color = 'yellow';
+  this.food = 'jelly';
+  this.honeyPot = 0;
+  // this.honeyPot++;
 };
 
+
+HoneyMakerBee.prototype = Object.create(Grub.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee; 
+
+
+HoneyMakerBee.prototype.makeHoney = function () {
+  this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function () {
+  this.honeyPot--;
+};
